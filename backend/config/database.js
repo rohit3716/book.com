3267f9b0121c931cd2a7d461337a387e0c542565
Rohit Raj <rohit.rajcs2020@indoreinstitute.com>
2024-01-08 02:07:37 +0530

    changing db config files

--- backend/config/database.js
+++ backend/config/database.js
@@ -2,10 +2,8 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 exports.connect = () => {
-    mongoose.connect( process.env.VITE_MONGODB_URL, {
-        useNewUrlParser:true,
-        useUnifiedTopology:true,
-    }).then(() => console.log("DB connected successfully"))
+    mongoose.connect( process.env.VITE_MONGODB_URL)
+    .then(() => console.log("DB connected successfully"))
     .catch( (error) => {
         console.log('Issue in DB Connection');
         console.error(error);
