8eb9e2847d185bed0b5002fe950149a581aaed79
Rohit Raj <rohit.rajcs2020@indoreinstitute.com>
2024-11-10 23:49:28 +0530

    added frontend deployed link

--- backend/index.js
+++ backend/index.js
@@ -1,46 +1,44 @@
 const express = require('express');
 const app = express();
 const database = require('./config/database');
-
 const userRoute = require("./routes/User");
 const bookRoute = require("./routes/Book");
-
-// const database = require('./config/database');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
-
 
 require('dotenv').config();
 const PORT = process.env.PORT || 4000;
 
-
-//DB connect
+// DB connect
 database.connect();
 
-//middlewares
+// Parse CORS origins from environment variable
+const allowedOrigins = process.env.CORS_ORIGIN.split(',');
+
+// Middlewares
 app.use(express.json());
 app.use(cookieParser());
 app.use(
     cors({
-        origin: JSON.parse(process.env.CORS_ORIGIN),
+        origin:process.env.CORS_ORIGIN,
         credentials: true,
         maxAge: 14400,
       })
 )
 
-//routes
+// Routes
 app.use("/api/v1/", userRoute);
 app.use('/api/v1/auth/', bookRoute);
 app.use('/api/v1/', bookRoute);
 
-//default route 
-app.get("/", ( req, res ) => {
+// Default route
+app.get("/", (req, res) => {
     return res.json({
-        success:true,
-        message:'Server is up and running....'
-    })
-})
+        success: true,
+        message: 'Server is up and running....'
+    });
+});
 
-app.listen( PORT, () => {
+app.listen(PORT, () => {
     console.log(`App is running at ${PORT}`);
-})
+});
